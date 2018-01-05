82b091bfce2ef8f7becfc1df9fc2423b97043756
yezi <18924400146@163.com>
2018-01-05 18:17:54 +0800

    sa

--- server/index.js
+++ server/index.js
@@ -47,8 +47,6 @@ Object.keys(proxyTable).forEach(function (context) {
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
   res.header('Access-Control-Allow-Credentials', true)
   next()
 })
