0f298d78e547fdf0ff45b2bd023ed14ad08f6939
yezi <18924400146@163.com>
2018-01-05 17:15:54 +0800

    修改服务器，增加数据库mongodb

--- server/index.js
+++ server/index.js
@@ -6,6 +6,31 @@ var https = require('https')
 var privateKey = fs.readFileSync('./server/private.pem', 'utf8')
 var certificate = fs.readFileSync('./server/file.crt', 'utf8')
 var credentials = {key: privateKey, cert: certificate}
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+var MongoStore = require('connect-mongo')(session)
+
+// 使用session
+app.use(cookieParser())
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 10 * 10 // 过期时间设置(单位毫秒)
+  },
+  store: new MongoStore({   // 创建新的mongodb数据库
+    url: 'mongodb://localhost/test'
+  })
+}))
+
+// // 解析post过来的数据
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// 引入编写好的api
+const api = require('./api.js')
+app.use(api)
 app.use(express.static('./dist'))
 
 var PORT = 9001
